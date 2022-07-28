162bd876122ce6b25e18520f63da4cedc338af75
Samu9523 <santin.samuel@gmail.com>
2022-07-27 23:25:18 -0500

    Form Baby UI

--- PruebaAmericaMovilSSZ/FormMyBaby/FormMyBabyView.swift
+++ PruebaAmericaMovilSSZ/FormMyBaby/FormMyBabyView.swift
@@ -11,6 +11,21 @@ import UIKit
 
 class FormMyBabyView: UIViewController {
 
+    @IBOutlet weak var avatarImage: UIImageView!
+    @IBOutlet weak var editAvatarImage: UIImageView!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthTextField: UITextField!
+    @IBOutlet weak var birthLabel: UILabel!
+    @IBOutlet weak var birthPickerImage: UIImageView!
+    @IBOutlet weak var girlButton: UIButton!
+    @IBOutlet weak var boyButton: UIButton!
+    @IBOutlet weak var cleanButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    
+    
+    
     // MARK: Properties
     var presenter: FormMyBabyPresenterProtocol?
 
